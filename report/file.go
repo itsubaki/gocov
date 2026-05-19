52ed628ac511988c341244079db497552eae9bbc
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-05-19 21:53:56 +0900

    Update NewFile

--- report/file.go
+++ report/file.go
@@ -27,12 +27,7 @@ func (f *File) SetLines(lines []Line) {
 	f.Stats.Increment(lines)
 }
 
-func NewFile(
-	rootPath string,
-	modulePath string,
-	profileFile string,
-	idx int,
-	blocks []profile.Block,
+func NewFile(rootPath, modulePath, profileFile string, idx int, blocks []profile.Block,
 ) (File, error) {
 	sourcePath, found := sourcePath(rootPath, modulePath, profileFile)
 	displayPath := displayPath(rootPath, modulePath, profileFile, sourcePath, found)
